602e772abd781aa3b8ecb8a2c7a971f8c82f376b
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-11 02:22:49 -0600

    feat: agregar estilos y estructura a la página de tienda

--- src/pages/tienda/tienda.js
+++ src/pages/tienda/tienda.js
@@ -1,7 +1,7 @@
 const $seccionCards = document.getElementById("seccion-cards");
 const createCards = (producto) => {
   return `
-    <div class="card h-100 p-2 m-2" style="width: 14rem">
+    <div class="card h-150 m-2" style="width: 14rem;background-color: #F2CC8F;">
         <img
         src="${producto.imagen}"
         class="card-img-top"
@@ -11,7 +11,6 @@ const createCards = (producto) => {
         <div class="card-body">
         <h5 class="card-title" id="nombre-producto">${producto.nombre}</h5>
         <p id="precio">${producto.precio}</p>
-        <a href="#" class="btn btn-secondary">me gusta</a>
         </div>
     </div>
 `;
