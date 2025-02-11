96d6a73aaf1b5ae81f79e220029b446605128f61
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-10 18:51:57 -0600

    feat: actualizar la visualización de productos en la tienda con generación dinámica de tarjetas

--- src/pages/tienda/tienda.js
+++ src/pages/tienda/tienda.js
@@ -1,13 +1,28 @@
-const $img = document.getElementById("image");
-const $precio = document.getElementById("precio");
-const $nombreProducto = document.getElementById("nombre-producto");
+const $seccionCards = document.getElementById("seccion-cards");
+const createCards = (producto) => {
+  return `
+ <div class="col mb-3">
+    <div class="card h-100 p-2" style="width: 14rem">
+        <img
+        src="${producto.imagen}"
+        class="card-img-top"
+        alt="Imagen de producto"
+        id="image"
+        />
+        <div class="card-body">
+        <h5 class="card-title" id="nombre-producto">${producto.nombre}</h5>
+        <p id="precio">${producto.precio}</p>
+        <a href="#" class="btn btn-primary">me gusta</a>
+        </div>
+    </div>
+ </div>
+`;
+};
 
 fetch("/public/json/productos.json")
   .then((productos) => productos.json())
   .then((productos) => {
-    for (producto of productos) {
-      $img.src = producto.imagen;
-      $precio.innerText = producto.precio;
-      $nombreProducto.innerText = producto.nombre;
-    }
+    $seccionCards.innerHTML = productos
+      .map((card) => createCards(card))
+      .join("");
   });
