6660480d665745eaeab7bbd798038cd93911f4be
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-11 22:52:31 -0600

    feat: agregar enlace a la imagen del producto y mejorar el cursor en las tarjetas de la tienda

--- src/pages/tienda/tienda.js
+++ src/pages/tienda/tienda.js
@@ -1,8 +1,10 @@
 const $seccionCards = document.getElementById("seccion-cards");
 const createCards = (producto) => {
   return `
+    
     <div class="card h-150 m-2" style="width: 14rem;background-color: #E07A5F;">
-        <img
+      <a>  
+      <img
         src="${producto.image}"
         class="card-img-top"
         alt="Imagen de producto"
@@ -13,6 +15,7 @@ const createCards = (producto) => {
         <p>${producto.descripcion}</p>
         <p id="precio">${producto.price}</p>
         </div>
+        </a>
     </div>
 `;
 };
