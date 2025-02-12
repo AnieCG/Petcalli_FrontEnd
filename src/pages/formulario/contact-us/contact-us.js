047d7685d37d0e08cb2f2a339ee70b27fbd54928
Aurelia <aurelia.roblesperez@gmail.com>
2025-02-12 17:16:23 -0600

    feat: revision de .js para el correo

--- src/pages/formulario/contact-us/contact-us.js
+++ src/pages/formulario/contact-us/contact-us.js
@@ -13,52 +13,38 @@
      // warning+=`El nombre no es valido <br>`    
 //}
 //})
-document.addEventListener("click", function () {
-  const form = document.getElementById('form');
-  const mensajeError = document.getElementById('mensajeError');
-
-  form.addEventListener('submit', function (event) {
-      event.preventDefault(); // Previene el envío del formulario para poder validarlo con Javascript
-
-      const nombre = document.getElementById('exampleInputName').value;
-      const correo = document.getElementById('exampleInputEmail').value;
-      const telefono = document.getElementById('exampleInputTel').value;
-      const mensaje = document.getElementById('exampleInputMessage').value;
+document.getElementById("form").addEventListener("submit", function {
+  e.preventDefault(); // Previene el envío del formulario para poder validarlo con JS
+      
+      const nombre = document.getElementById('exampleInputName').value.trim();
+      const correo = document.getElementById('exampleInputEmail').value.trime();
+      const telefono = document.getElementById('exampleInputTel').value.trime();
+      const mensaje = document.getElementById('exampleInputMessage').value.trime();
+      const mensajeError = document.getElementById('mensajeError');
 
       // Validación de informacion que se proporcione
-      if (nombre.trim() === '') {
-          mensajeError.innerText = 'El campo nombre no puede estar vacío.';
+      if (nombre === '' || correo ==`` || telefono == `` || mensaje ==`` ){
+          mensajeError.innerText = 'El campo no puede estar vacío.';
           return;
-      }
-
-      if (correo.trim() === '') {
-          mensajeError.innerText = 'El campo email no puede estar vacío.';
-          return;
-      }
-      if (telefono.trim() === '') {
-        mensajeError.innerText= 'El campo telefono no puede estar vacío.';
+      };
+      if (!/^\d+$/.test(telefono)) {
+        mensajeError.innerText = 'El campo teléfono solo puede contener números.';
         return;
-    }
-
-      if (!validarEmail(email)) {
-          mensajeError.innerText = 'El formato del correo no es válido.';
-          return;
-      }
-
-      if (mensaje === '') {
-          mensajeError.innerText = 'Debes escribir un mensaje.';
-          return;
-      }
-
-      // Si todo está correcto, se puede proceder a enviar el formulario o hacer alguna otra acción
-      mensajeError.innerText = '';
-      // alert('Formulario enviado con éxito!');
-      formulario.submit(); // Descomentar esta línea para permitir el envío del formulario
-  });
-
-  function validarEmail(exampleInputEmail) {
-      const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      return re.test(String(exampleInputEmail).toLowerCase());
-  }
-});  
-
+      };
+      const templateParams = {
+        nombre: exampleInputName,
+        correo: exampleInputEmail,
+        telefono: exampleInputTel,
+        mensaje: exampleInputMessage
+        
+    };
+    emailjs.send('TU_ID_DE_SERVICIO', 'TU_ID_DE_PLANTILLA', templateParams)
+        .then(function(response) {
+           console.log('Correo enviado', response.status, response.text);
+           alert('Los datos fueron enviados correctamente.');
+        }, 
+        function(error) {
+           console.log('Fallo en el envío', error);
+           alert('Hubo un problema al enviar los datos.');
+        });
+});
