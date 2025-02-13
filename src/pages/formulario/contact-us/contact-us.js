0ec7e9d22f0e2b1877e89287288022ccb3fb5daa
Aurelia <aurelia.roblesperez@gmail.com>
2025-02-12 20:14:32 -0600

    feat= revision de para estructurar el .js

--- src/pages/formulario/contact-us/contact-us.js
+++ src/pages/formulario/contact-us/contact-us.js
@@ -13,23 +13,23 @@
      // warning+=`El nombre no es valido <br>`    
 //}
 //})
-document.getElementById("form").addEventListener("submit", function {
-  e.preventDefault(); // Previene el envío del formulario para poder validarlo con JS
-      
+//Accedienco al elemento Id del HTML, aqui va el escucha del evento
+document.getElementById("form").addEventListener("submit", function (event){
+  event.preventDefault(); // Previene el envío del formulario para poder validarlo con JS
+      //declaro las variables para aceder al DOM que esta en el html.
       const nombre = document.getElementById('exampleInputName').value.trim();
       const correo = document.getElementById('exampleInputEmail').value.trime();
       const telefono = document.getElementById('exampleInputTel').value.trime();
       const mensaje = document.getElementById('exampleInputMessage').value.trime();
       const mensajeError = document.getElementById('mensajeError');
 
-      // Validación de informacion que se proporcione
+      // Validación de informacion que se proporcione, con el operador OR, se comprobara que los campos no esten vacios.
       if (nombre === '' || correo ==`` || telefono == `` || mensaje ==`` ){
-          mensajeError.innerText = 'El campo no puede estar vacío.';
+          mensajeError.innerText = 'El campo no puede estar vacío.';//para que el usuario complete antes de continuar
           return;
       };
       if (!/^\d+$/.test(telefono)) {
-        mensajeError.innerText = 'El campo teléfono solo puede contener números.';
-        return;
+        mensajeError.innerText = 'El campo teléfono solo puede contener números.';//verifica que la cadena tenga solo digitos,
       };
       const templateParams = {
         nombre: exampleInputName,
