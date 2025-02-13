59f30806fdd799bc9a9f32fe8498e4e24e27eb5b
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-13 16:32:30 -0600

    Feat: Añadir validación de formularios y navegación dinámica, actualizar estilos en la tienda

--- src/pages/tienda/tienda.js
+++ src/pages/tienda/tienda.js
@@ -2,7 +2,7 @@ const $seccionCards = document.getElementById("seccion-cards");
 const createCards = (producto) => {
   return `
     
-    <div class="card h-150 m-2" style="width:15rem;">
+    <div class="card h-150 m-2" style="width:13rem;">
       <img
         src="${producto.image}"
         class="card-img-top"
@@ -13,6 +13,7 @@ const createCards = (producto) => {
         <h5 class="card-title" id="nombre-producto">${producto.title}</h5>
         <p>${producto.descripcion}</p>
         <p id="precio">${producto.price}</p>
+        <button class="btn btn-warning">Añadir a carrito</button>
         </div>
     </div>
 `;
