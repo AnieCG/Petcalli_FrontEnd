56995c1c1dcbd4228231e516ea40cff43426a549
AnieCG <qfb.anacg@gmail.com>
2025-02-13 23:05:55 -0600

    feat: se corrige botón de compra en header

--- src/modules/header/header.js
+++ src/modules/header/header.js
@@ -6,14 +6,14 @@ const insertHeader = () => {
       <div class="container">
         <div class="row">
           <div class="col-12 col-lg-6 d-flex flex-column pt-5">
-            <h1 class="text-center text-md-center text-lg-start">Petcalli</h1>
+            <h1 class="text-centerd-center text-lg-start">Petcalli</h1>
             <p class="mx-auto mx-md-auto mx-lg-0 text-center text-md-center text-lg-start fs-4 col-9 pt-3">
               "Porque en el corazón de nuestra tienda está el compromiso de
               cuidar a tu mascota tanto como tú lo haces”
             </p>
             <div class="pt-3 text-center text-md-center text-lg-start">
               <a href="#">
-                <button type="button" id="botonCompra" class="btn btn-primary btn-lg fs-5 text">
+                <button type="button" id="botonCompra" class="btn btn-primary btn-lg fs-5 text mb-4">
                   Comprar
                 </button>
               </a>
