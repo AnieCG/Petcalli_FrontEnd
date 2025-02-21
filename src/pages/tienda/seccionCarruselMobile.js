69929e5f55532fa3656eb88e9fe0874e890b644a
Erika Gonzalez <erikagov0926@gmail.com>
2025-02-20 18:00:42 -0600

    filtrado por tag agregado

--- src/pages/tienda/seccionCarruselMobile.js
+++
@@ -1,7 +0,0 @@
-const $seccionCarrusel = document.getElementById("section-carrusel-mobile");
-
-const createCarrusel = () => {
-  return ``;
-};
-
-$seccionCarrusel.innerHTML = createCarrusel();
--- src/pages/tienda/tienda.js
+++ src/pages/tienda/tienda.js
@@ -115,7 +115,9 @@ const createCards = (producto) => {
 
    fetch("/public/json/productos.json")
     .then((productos) => productos.json())
-    .then((productos) => {
+    .then((productos) => { console.log("estoy en el json")
+      console.log(productos);
+      console.log(typeof(productos));
       $seccionCards.innerHTML = productos
       .map((card) => createCards(card))
       .join("");
@@ -204,6 +206,68 @@ const filterProductsByCategory = (category) => {
 
 
 // Seccion de filtrado por tags
-
-
   
+//      Botones de los tags
+const tagJuguetes = document.getElementById("tagjuguetes");
+const tagAlimento = document.getElementById("tagAlimento");
+const tagNatural = document.getElementById("tagNatural");
+const tagSalmón = document.getElementById("tagSalmón");
+const tagCatnip = document.getElementById("tagCatnip");
+const tagArnés = document.getElementById("tagArnés");
+const tagCachorro = document.getElementById("tagCachorro");
+const tagRueda = document.getElementById("tagRueda");
+
+ //     Funcion que me selecciona crea una lista  
+function lista(url) { ///"public/json/productos.json"
+  let listaDeTodo = [];
+  fetch(url)
+            .then((productos) => productos.json())
+            .then((productos) => {
+    listaDeTodo.unshift(productos);
+    })
+return listaDeTodo;
+}
+
+const listaProductos = lista("/public/json/productos.json");
+//console.log(listaProductos);
+
+// funcion que filtrado
+const filtroTag = (tag, lista = []) =>{
+  let listaFiltrada = (lista.filter(product => product.description.includes(tag) || product.title.includes(tag)||product.category.includes(tag)));
+  return listaFiltrada;
+}
+
+
+//   Acciones de los tags
+tagJuguetes.addEventListener( "click", () => {
+  let juguetes = filtroTag("Juguetes", listaProductos[0]);
+  $seccionCards.innerHTML = juguetes.map((card) => createCards(card)).join("");
+});
+tagAlimento.addEventListener( "click", () => {
+  let tag = filtroTag("Alimento", listaProductos[0]);
+  $seccionCards.innerHTML = tag.map((card) => createCards(card)).join("");
+});
+tagNatural.addEventListener( "click", () => {
+  let tag = filtroTag("Natural", listaProductos[0]);
+  $seccionCards.innerHTML = tag.map((card) => createCards(card)).join("");
+});
+tagSalmón.addEventListener( "click", () => {
+  let tag = filtroTag("Salm", listaProductos[0]);
+  $seccionCards.innerHTML = tag.map((card) => createCards(card)).join("");
+});
+tagCatnip.addEventListener( "click", () => {
+  let tag = filtroTag("Catnip", listaProductos[0]);
+  $seccionCards.innerHTML = tag.map((card) => createCards(card)).join("");
+});
+tagArnés.addEventListener( "click", () => {
+  let tag = filtroTag("Arn", listaProductos[0]);
+  $seccionCards.innerHTML = tag.map((card) => createCards(card)).join("");
+});
+tagCachorro.addEventListener( "click", () => {
+  let tag = filtroTag("Cachorro", listaProductos[0]);
+  $seccionCards.innerHTML = tag.map((card) => createCards(card)).join("");
+});
+tagRueda.addEventListener( "click", () => {
+  let tag = filtroTag("Rueda", listaProductos[0]);
+  $seccionCards.innerHTML = tag.map((card) => createCards(card)).join("");
+});
