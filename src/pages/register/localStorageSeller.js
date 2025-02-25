79482c3e17c9dcf770d5808770c0b5adf8a317fd
Valeria Aceves <vale.acevesschz@gmail.com>
2025-02-24 23:02:19 -0600

    feat: se hizo la misma validacion del login y register del usuario para las personas que se registran como vendedores, usando el link en el footer

--- src/pages/register/localStorageSeller.js
+++ src/pages/register/localStorageSeller.js
@@ -7,6 +7,16 @@ document
     const lastName = document.getElementById("last-name").value.trim();
     const email = document.getElementById("emailDueño").value.trim();
     const telephone = document.getElementById("telefonoDueño").value.trim();
+    const nameStore = document.getElementById("nombreTienda").value.trim();
+    const description = document.getElementById("descripcionTienda").value.trim();
+    const address = document.getElementById("address").value.trim();
+    const zip = document.getElementById("Zip").value.trim();
+    const city = document.getElementById("City").value.trim();
+    const state = document.getElementById("State").value.trim();
+
+
+
+
     const password = document.getElementById("password").value.trim();
     const user = {
       name,
@@ -14,6 +24,12 @@ document
       email,
       password,
       telephone,
+      nameStore,
+      description,
+      zip,
+      address,
+      state,
+      city,
     };
 
     localStorage.setItem("user", JSON.stringify(user));
