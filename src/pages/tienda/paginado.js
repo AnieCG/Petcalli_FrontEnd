750cdea7a9b0ac99939c2ff1042fe0a085c15082
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-25 22:16:21 -0600

    Refactor: extrae la función de creación de tarjetas a un nuevo archivo

--- src/pages/tienda/paginado.js
+++ src/pages/tienda/paginado.js
@@ -1,4 +1,4 @@
-
+import createCards from "./createCards.js";
 // Variables globales
 const itemsPerPage = 6;
 let currentPage = 1;
@@ -11,23 +11,6 @@ const $counterProductsToShow = document.getElementById("counterProductsToShow");
 const $prevPageButton = document.getElementById("prevPage");
 const $nextPageButton = document.getElementById("nextPage");
 const $pageNumberDisplay = document.getElementById("pageNumber");
-
-// Función para crear una tarjeta de producto
-const createCards = (producto) => {
-  return `
-    <div class="card h-150 m-2" style="width:16rem;">
-      <img src="${producto.image}" class="card-img-top" alt="Imagen de producto" id="image" />
-      <div class="card-body">
-        <h5 class="card-title" id="nombre-producto">${producto.title}</h5>
-        <p>${producto.description}</p>
-        <p id="precio">${producto.price}</p>
-        <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#${producto.id}">
-          Ver producto
-        </button>
-      </div>
-    </div>
-  `;
-};
 
 // Función para mostrar los productos de una página específica
 function showPage(page, productos) {
