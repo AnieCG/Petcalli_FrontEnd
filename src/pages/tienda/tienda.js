32484476e573335f3d8dde6793a40dff77b628c4
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-02-27 00:44:48 -0600

    Refactor: mejora la obtención de productos y actualiza el filtrado en tienda.js

--- src/pages/tienda/tienda.js
+++ src/pages/tienda/tienda.js
@@ -2,16 +2,26 @@ import filtradoPorPrecio from "./funciones-filtrados/filtradoPrecio.js";
 import getJson from "./funciones-filtrados/getProducts.js";
 import filterProductsByPetType from "./funciones-filtrados/filtrado-petType.js";
 import insertCarruselMasPopulares from "./components/carruselMasPopulares.js";
+const $seccionCards = document.getElementById("seccion-cards");
+import createCards from "./components/createCards.js";
+
+insertCarruselMasPopulares();
+
+let products = [];
+let filterProducts = [];
+async function getProducts() {
+  const plainProducts = await fetch("/public/json/productos.json");
+  const objectProducts = await plainProducts.json();
+  products = objectProducts;
+}
+
+await getProducts();
+filtradoPorPrecio(products);
+
+filterProductsByPetType(products);
 
 document.addEventListener("DOMContentLoaded", async () => {
-  const productos = await getJson("/public/json/productos.json");
-  const allProducts = await getJson("/public/json/productos.json");
+  // const productos = await getJson("/public/json/productos.json");
+  // const allProducts = await getJson("/public/json/productos.json");
   //Insertar carrusel
-  insertCarruselMasPopulares();
-
-  /*FILTRADO POR PRECIO */
-  filtradoPorPrecio(productos);
-
-  /*FILTRADO POR TIPO DE MASCOTA */
-  filterProductsByPetType(productos);
 });
