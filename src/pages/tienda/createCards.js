6be40d6fbf9130e3f5fc8b2fe98c2bfbf2145677
Erika Gonzalez <erikagov0926@gmail.com>
2025-02-26 19:57:22 -0600

    fondo igual en smarthphone

--- src/pages/tienda/createCards.js
+++ src/pages/tienda/createCards.js
@@ -1,13 +1,27 @@
 export default function createCards(producto) {
   return `
-    <div class="card h-150 m-1" id="product-card">
+    <div class="card  m-1" id="product-card">
       <img
         src="${producto.image}"
-        class="card-img-top img-fluid"
+        class="card-img-top img-fluid d-block d-sm-none"
+        alt="Imagen de producto"
+        id="image2" 
+        />
+      <img
+        src="${producto.image}"
+        class="card-img-top img-fluid d-none d-sm-block"
         alt="Imagen de producto"
         id="image" 
         />
-        <div class="card-body" id="body">
+  
+        <div class="card-body d-block d-sm-none" id="body2">
+          <h6 class="card-title" id="nombre-producto">${producto.title}</h6>
+          <p id="description" ><small>${producto.description}</small></p>
+          <p id="precio" >${producto.price}</p>
+          <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#${producto.id}"
+          >Ver producto</button>
+        </div>
+        <div class="card-body d-none d-sm-block" id="body">
           <h6 class="card-title" id="nombre-producto">${producto.title}</h6>
           <p id="description" ><small>${producto.description}</small></p>
           <p id="precio" >${producto.price}</p>
