6bcdce45fbf8a225409ea3aa7a9dfd6845401a28
Erika Gonzalez <erikagov0926@gmail.com>
2025-02-26 20:13:06 -0600

    alineacion un poco corregida

--- src/pages/tienda/createCards.js
+++ src/pages/tienda/createCards.js
@@ -15,9 +15,9 @@ export default function createCards(producto) {
         />
   
         <div class="card-body d-block d-sm-none" id="body2">
-          <h6 class="card-title" id="nombre-producto">${producto.title}</h6>
-          <p id="description" ><small>${producto.description}</small></p>
-          <p id="precio" >${producto.price}</p>
+          <h5 class="card-title" id="nombre-producto mb-5">${producto.title}</h5>
+          <p id="description" class="align-middle mb-5">${producto.description}</p>
+          <p id="precio" class="mt-5">${producto.price}</p>
           <button type="button" class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#${producto.id}"
           >Ver producto</button>
         </div>
