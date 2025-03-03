39a54db379ddcb4a6623cad6486cad8374c8ba5c
Erika Gonzalez <erikagov0926@gmail.com>
2025-03-02 21:09:58 -0600

    contador

--- src/pages/tienda/contador.js
+++ src/pages/tienda/contador.js
@@ -1,6 +1,11 @@
-let contadorPiezasMenos = document.getElementsByClassName("menos1");
-console.log((contadorPiezasMenos));
-let contadorIndividual = 0; //getElementsByClassName
+
+
+setTimeout(() => {
+    console.log("1 Segundo esperado")
+    let contadorPiezasMenos = document.getElementsByClassName("menos1");
+    console.log((contadorPiezasMenos));
+    let contadorIndividual = 0; //getElementsByClassName
+  }, 1000);
 
 contadorPiezasMenos[1].addEventListener("click", ()=>{ 
     contadorIndividual --;
