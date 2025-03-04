b85705e070ec91ae9a454532074b89fd3ace1c66
FernandoAlexisHH <fer_guitarrista@hotmail.com>
2025-03-04 00:58:11 -0600

    Feat: Agrega funcionalidad para almacenar productos en localStorage al hacer clic en "Comprar ahora"

--- src/pages/tienda/tienda.js
+++ src/pages/tienda/tienda.js
@@ -3,7 +3,7 @@ import insertCarruselMasPopulares from "./components/carruselMasPopulares.js";
 import filtradoTag from "./funciones-filtrados/filtroTag.js";
 import mostrarProductos from "./funciones-filtrados/mostrarProductos.js";
 import insertQueryProducts from "./funciones-filtrados/insertQueryProducts.js";
-
+import setItemLocalStorage from "./funciones-filtrados/setItemLocalStorage.js";
 insertCarruselMasPopulares();
 
 const $span = document.getElementsByClassName("price-value");
@@ -28,42 +28,66 @@ let selectedFilters = {
   petType: null,
   category: [],
   brand: [],
-  price: null
+  price: null,
 };
+//cargar elementos al local storage
+document.addEventListener("click", (event) => {
+  setItemLocalStorage(event, products);
+});
+
+const usuarioGuardado = JSON.parse(localStorage.getItem("producto"));
+console.log(usuarioGuardado);
 
 console.log(window.location.href);
 function updateFilters() {
-  let filteredProducts = products.filter(product => {
-    return (!selectedFilters.petType || product.petType === selectedFilters.petType) &&
-    (!selectedFilters.category.length || selectedFilters.category.includes(product.category)) &&
-    (!selectedFilters.brand.length || selectedFilters.brand.includes(product.marca)) &&
-    (!selectedFilters.price || parseFloat(product.price.replace(/[$,]/g, "")) <= parseFloat(selectedFilters.price));
+  let filteredProducts = products.filter((product) => {
+    return (
+      (!selectedFilters.petType ||
+        product.petType === selectedFilters.petType) &&
+      (!selectedFilters.category.length ||
+        selectedFilters.category.includes(product.category)) &&
+      (!selectedFilters.brand.length ||
+        selectedFilters.brand.includes(product.marca)) &&
+      (!selectedFilters.price ||
+        parseFloat(product.price.replace(/[$,]/g, "")) <=
+          parseFloat(selectedFilters.price))
+    );
   });
-  
+
   mostrarProductos(filteredProducts);
   counterProductsToShow.innerHTML = filteredProducts.length;
-};
+}
 
 // Eventos que activan los filtros en tiempo real
 
-document.querySelectorAll(".form-range")[1].addEventListener("input", (event) => {
-  selectedFilters.price = event.target.value ? parseFloat(event.target.value) : null;
-  let maxPrice = parseInt(event.target.value);
-  $span[1].innerHTML = `$ ${maxPrice}`;
-  console.log(selectedFilters);
-  updateFilters();
-});
-document.querySelectorAll(".form-range")[0].addEventListener("input", (event) => {
-  selectedFilters.price = event.target.value ? parseFloat(event.target.value) : null;
-  let maxPrice = parseInt(event.target.value);
-  $span[0].innerHTML = `$ ${maxPrice}`;
-  console.log(selectedFilters);
-  updateFilters();
-});
+document
+  .querySelectorAll(".form-range")[1]
+  .addEventListener("input", (event) => {
+    selectedFilters.price = event.target.value
+      ? parseFloat(event.target.value)
+      : null;
+    let maxPrice = parseInt(event.target.value);
+    $span[1].innerHTML = `$ ${maxPrice}`;
+    console.log(selectedFilters);
+    updateFilters();
+  });
+document
+  .querySelectorAll(".form-range")[0]
+  .addEventListener("input", (event) => {
+    selectedFilters.price = event.target.value
+      ? parseFloat(event.target.value)
+      : null;
+    let maxPrice = parseInt(event.target.value);
+    $span[0].innerHTML = `$ ${maxPrice}`;
+    console.log(selectedFilters);
+    updateFilters();
+  });
 
 document.querySelectorAll(".petCategory").forEach((button) => {
   button.addEventListener("click", (event) => {
-    document.querySelectorAll(".petCategory").forEach(btn => btn.classList.remove("selected"));
+    document
+      .querySelectorAll(".petCategory")
+      .forEach((btn) => btn.classList.remove("selected"));
     selectedFilters.petType = event.currentTarget.value || null;
     event.currentTarget.classList.add("selected");
     updateFilters();
@@ -75,7 +99,9 @@ document.querySelectorAll(".brand").forEach((checkbox) => {
     if (event.target.checked) {
       selectedFilters.brand.push(event.target.value);
     } else {
-      selectedFilters.brand = selectedFilters.brand.filter(brand => brand !== event.target.value);
+      selectedFilters.brand = selectedFilters.brand.filter(
+        (brand) => brand !== event.target.value
+      );
     }
     updateFilters();
   });
@@ -86,7 +112,9 @@ document.querySelectorAll(".category").forEach((checkbox) => {
     if (event.target.checked) {
       selectedFilters.category.push(event.target.value);
     } else {
-      selectedFilters.category = selectedFilters.category.filter(category => category !== event.target.value);
+      selectedFilters.category = selectedFilters.category.filter(
+        (category) => category !== event.target.value
+      );
     }
     updateFilters();
   });
@@ -96,9 +124,11 @@ document.querySelectorAll(".tags").forEach((input) => {
   input.addEventListener("click", () => {
     filterProducts = filtradoTag(filterProducts, input);
     mostrarProductos(filterProducts);
-    counterProductsToShow.innerHTML = filterProducts.length;    
+    counterProductsToShow.innerHTML = filterProducts.length;
     filterProducts = [...products];
   });
 });
-document.addEventListener("DOMContentLoaded", insertQueryProducts(filterProducts));
-
+document.addEventListener(
+  "DOMContentLoaded",
+  insertQueryProducts(filterProducts)
+);
