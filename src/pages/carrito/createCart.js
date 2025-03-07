7ab4bb62f72dcfb5616e2a3b4c00e0d8186b667a
Alan Flores <alanpfqa@gmail.com>
2025-03-06 22:44:42 -0600

    Fix estilos de tarjetas de carrito

--- src/pages/carrito/createCart.js
+++ src/pages/carrito/createCart.js
@@ -27,20 +27,33 @@
             //no aparece la seccion en el html, 
             /* Como decias llamas mal a los atributos, ponias producto, y es con el metodo element */
             nuevoProducto.innerHTML = `
-            <img src=${element.image}/>
-            <h5>${element.title}</h5>
-            <p>${element.description}</p>
-            <p>${element.price}</p> 
-            <button id="decrease-btn"><i class="fas fa-arrow-down"></i></button>
-                        <span id="counter-value">1</span>
-            <button id="increase-btn"><i class="fas fa-arrow-up"></i></button>
-            <button class="btn-delete" onclick="handleDelete()">
-                      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
-                        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
-                        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
-                      </svg>
-            </button>    
-            `
+            <div class="row justify-content-center align-items-center">
+                <div class="col-12 col-md-3 text-center">
+                    <img src=${element.image} class="img-fluid" alt="${element.title}"/>
+                </div>
+                <div class="col-12 col-md-3 text-center">
+                    <h5>${element.title}</h5>
+                </div>
+                <div class="col-12 col-md-2 text-center">
+                    <p>${element.price}</p> 
+                </div>
+                <div class="col-12 col-md-2 text-center">
+                    <div class="d-flex justify-content-center align-items-center">
+                        <button id="decrease-btn" class="btn btn-outline-secondary"><i class="fas fa-arrow-down"></i></button>
+                        <span id="counter-value" class="mx-2">1</span>
+                        <button id="increase-btn" class="btn btn-outline-secondary"><i class="fas fa-arrow-up"></i></button>
+                    </div>
+                </div>
+                <div class="col-12 col-md-2 text-center">
+                    <button class="btn btn-danger btn-delete" onclick="handleDelete()">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash" viewBox="0 0 16 16">
+                            <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+                            <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+                        </svg>
+                    </button>    
+                </div>
+            </div>
+        `;
             contenedorTarjetas.appendChild(nuevoProducto); //cuando la tarjeta este lista de nuevoProducto se deb agregar al contenedor principal, contenedorTarjetas
      nuevoProducto
      //la funcion de los botones
