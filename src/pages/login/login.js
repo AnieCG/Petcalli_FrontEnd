f22e111f559b39528b75d05b6cc59b08a5f82dcc
Valeria Aceves <vale.acevesschz@gmail.com>
2025-03-21 14:05:43 -0600

    cambios en front login

--- src/pages/login/login.js
+++ src/pages/login/login.js
@@ -1,26 +1,39 @@
-document.getElementById("loginForm").addEventListener("submit", function (event) {
-    event.preventDefault();
+document.getElementById("loginForm").addEventListener("submit", async function (event) {
+  event.preventDefault();
 
-    const emailLogin = document.getElementById("email").value.trim();
-    const passwordLogin = document.getElementById("password").value.trim();
-    const usuarioGuardado = JSON.parse(localStorage.getItem("user"));
+  const emailLogin = document.getElementById("email").value.trim();
+  const passwordLogin = document.getElementById("password").value.trim();
 
-    if (!usuarioGuardado) {
-      alert("No hay usuarios registrados.");
-      return;
-    }
+  try {
+    
+      const response = await fetch("http://localhost:8080/api/v1/users/login", {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json'
+          },
+          body: JSON.stringify({
+              email: emailLogin,
+              password: passwordLogin
+          })
+      });
 
-    if (
-      usuarioGuardado.email === emailLogin &&
-      usuarioGuardado.password === passwordLogin
-    ) {
-      alert("Inicio de sesión exitoso!");
-      localStorage.setItem("isLoggedIn", "true");
-      window.location.href = "/Index.html";
-    } else {
-      alert("Email o contraseña incorrectos.");
-    }
-
-
+      if (response.ok) {
+          const usuario = await response.json();
+          
+  
+          if (usuario.email === emailLogin && usuario.password === passwordLogin) {
+              alert("Inicio de sesión exitoso!");
+              localStorage.setItem("isLoggedIn", "true");
+              localStorage.setItem("user", JSON.stringify(usuario)); // Guarda el usuario en localStorage si es necesario
+              window.location.href = "/Index.html";
+          } else {
+              alert("Email o contraseña incorrectos.");
+          }
+      } else {
+          alert("Error en el inicio de sesión. Verifica tus credenciales.");
+      }
+  } catch (error) {
+      console.error("Error al realizar la solicitud:", error);
+      alert("Ocurrió un error en el servidor.");
+  }
 });
-
